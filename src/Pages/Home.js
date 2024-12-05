21ba44cf9f5fc8c98f84e31ff7715523bc69f5ec
LucasGdSToledo <164894386+LucasGdSToledo@users.noreply.github.com>
2024-12-05 03:36:00 +0000

    arrumando botão, transição de texto

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './Home.css';
-// import bombeiros from './images/bombeiros.png';
+import bombeiros from './images/bombeiros.png';
 import {Link, Outlet} from "react-router-dom";
 const Home = () => {
+  
+  {/* começo de transição */}
+  const [style, setStyle] = useState({
+    transform: "translateX(-10%)",
+    opacity: 0, 
+    transition: "transform 1s ease, opacity 3s ease", 
+  });
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setStyle({
+        transform: "translateX(0)", 
+        opacity: 1, 
+        transition: "transform 1s ease, opacity 2s ease", 
+      });
+    }, 100); 
+
+    return () => clearTimeout(timer); 
+  }, []);
+{/* fim de transição */}
   return (
     <div className="App">
 
@@ -25,10 +45,10 @@ const Home = () => {
             importantes para que possamos agir rapidamente. Cada denúncia conta e
             ajuda a preservar nosso ecossistema.
           </p>
-          <button className="report-button">Reportar</button>
+          <Link to="/Stories.js" className="report-button">Reportar</Link>
         </div>
-        {/* <img src={bombeiros} alt="bombeiros" className="report-image"
-        /> */}
+         <img src={bombeiros} alt="bombeiros" className="report-image"/>
+        
       </section>
     </div>
   );
