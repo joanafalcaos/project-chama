b0ab228894bf241e999ce2a8384834a076775182
Gomes <joyce.gomes@accenture.com>
2024-12-05 14:32:05 -0300

    Tela de adicionar relato - pt. 1

--- src/Components/AddStory.js
+++ src/Components/AddStory.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './AddStory.css';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { TextField, Button, Box } from '@mui/material';
-import UploadIcon from '@mui/icons-material/Upload';
 
 const theme = createTheme({
   palette: {
@@ -13,39 +12,60 @@ const theme = createTheme({
 });
 
 function AddStory() {
-  const [selectedFile, setSelectedFile] = useState(null);
   const [relato, setRelato] = useState('');
-
-  const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
-  };
+  const [userId] = useState(1); // userId constante
+  const [address, setAddress] = useState({
+    zipCode: '',
+    street: '',
+    houseNumber: '',
+    neighborhood: '',
+    city: '',
+  });
 
   const handleRelatoChange = (event) => {
     setRelato(event.target.value);
   };
 
+  const handleAddressChange = (event) => {
+    const { name, value } = event.target;
+    setAddress((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
-    // console.log('Formulário enviado!');
 
-    const formData = new FormData();
-    formData.append('description', relato);
-
-    if (selectedFile) {
-      formData.append('image', selectedFile);
-    }
+    const data = {
+      description: relato,
+      userId,
+      address,
+    };
 
     try {
-      const response = await fetch('http://localhost:8080/api/reports/addReport', {
+      const response = await fetch('http://localhost:8080/api/reports', {
         method: 'POST',
-        body: formData,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
       });
 
-      // if (response.ok) {
-      //   console.log('Relato enviado com sucesso!');
-      // } else {
-      //   console.error('Erro ao enviar o relato:', response.statusText);
-      // }
+      if (response.ok) {
+        console.log('Relato enviado com sucesso!');
+
+        setRelato('');
+        setAddress({
+          zipCode: '',
+          street: '',
+          houseNumber: '',
+          neighborhood: '',
+          city: '',
+        });
+      } else {
+        console.error('Erro ao enviar o relato:', response.statusText);
+      }
     } catch (error) {
       console.error('Erro de rede:', error);
     }
@@ -55,9 +75,9 @@ function AddStory() {
     <div className="story-container">
       <h3>Identificou um foco de incêndio ou queimada? Relate aqui!</h3>
       <ThemeProvider theme={theme}>
-        <Box 
-          component="form" 
-          onSubmit={handleSubmit} 
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
           className="form-container"
           style={{
             display: 'flex',
@@ -68,6 +88,46 @@ function AddStory() {
           }}
         >
           <TextField
+            label="CEP"
+            variant="outlined"
+            fullWidth
+            name="zipCode"
+            value={address.zipCode}
+            onChange={handleAddressChange}
+          />
+          <TextField
+            label="Rua"
+            variant="outlined"
+            fullWidth
+            name="street"
+            value={address.street}
+            onChange={handleAddressChange}
+          />
+          <TextField
+            label="Número"
+            variant="outlined"
+            fullWidth
+            name="houseNumber"
+            value={address.houseNumber}
+            onChange={handleAddressChange}
+          />
+          <TextField
+            label="Bairro"
+            variant="outlined"
+            fullWidth
+            name="neighborhood"
+            value={address.neighborhood}
+            onChange={handleAddressChange}
+          />
+          <TextField
+            label="Cidade"
+            variant="outlined"
+            fullWidth
+            name="city"
+            value={address.city}
+            onChange={handleAddressChange}
+          />
+          <TextField
             label="Relato"
             variant="outlined"
             fullWidth
@@ -77,31 +137,9 @@ function AddStory() {
             rows={4}
             onChange={handleRelatoChange}
           />
-
-          <input 
-            accept="image/*" 
-            style={{ display: 'none' }} 
-            id="upload-photo" 
-            type="file" 
-            onChange={handleFileChange}
-          />
-          <label htmlFor="upload-photo">
-            <Button 
-              variant="contained" 
-              color="primary" 
-              component="span"
-              className="button"
-              startIcon={<UploadIcon />}
-            >
-              Upload de Foto
-            </Button>
-          </label>
-
-          {selectedFile && <p className='img-name'>Arquivo selecionado: {selectedFile.name}</p>}
-
-          <Button 
-            type="submit" 
-            variant="contained" 
+          <Button
+            type="submit"
+            variant="contained"
             color="primary"
             className="button"
           >
